c6f5e8e99c358a6e35682e00ec9025672dea8b1e
evoup <evoex123@gmail.com>
2016-12-13 18:29:02 +0800

    add code

--- count.py
+++ count.py
@@ -1,1 +1,43 @@
+
+# -*- coding: utf-8 -*-
 import os
+from collections import defaultdict
+
+no_op = 0
+score = defaultdict(int)
+file_object = open(os.getcwd() + '/countLog.txt', 'w+')
+
+def convertToDict(fname):
+    with open(fname) as f:
+        idfas = f.readlines()
+        a =[]
+        for idfa in idfas:
+            idfa = idfa.strip("\n")
+            a.append(idfa)
+        return a
+
+def makeScore(key):
+    global score
+    score[key] += 1
+
+
+def searchSameLine(fname, dic):
+    with open(fname) as f:
+        targetLines = f.readlines()
+        for targetLine in targetLines:
+            targetLine = targetLine.strip("\n")
+            if targetLine in dic:
+                makeScore(targetLine)
+
+
+#把小文件加入dict
+dict = convertToDict(os.getcwd() + "/dataset/test.idfa.csv")
+
+#寻找大文件中命中dict的项目
+searchSameLine(os.getcwd() + "/dataset/test.000000_0.0", dict)
+
+
+#把相同的项目打印到结果文件中
+file_object.write("2个文件中相同的行如下:\n")
+for item in score:
+    file_object.write(item + "\n")
